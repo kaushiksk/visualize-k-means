2ba3978b1e54f52403700f49037f0004f66366fe
kaushiksk <kaushikskalmady@gmail.com>
2017-06-10 18:09:34 +0530

    Inline comments

--- kmeansvisualize.py
+++ kmeansvisualize.py
@@ -5,44 +5,6 @@ import numpy as np
 import random
 from six.moves import input
 
-def plotme(data, target, centroids=[], legends=False, iter=None):
-	plt.clf()
-
-
-	class_1 = data[target==0]
-	class_2 = data[target==1]
-	class_3 = data[target==2]
-
-
-	
-	if legends:
-		c1 = plt.scatter(class_1[:,0], class_1[:,1],c='m',
-			    marker='+')
-	
-		c2 = plt.scatter(class_2[:,0], class_2[:,1],c='m',
-			    marker='o')
-	
-
-		c3 = plt.scatter(class_3[:,0], class_3[:,1],c='m',
-			    marker='*')
-		plt.legend([c1, c2, c3], ['Setosa', 'Versicolor',
-	    			'Virginica'])
-		plt.title('Iris dataset with 3 clusters and known outcomes')
-	else:
-		c1 = plt.scatter(class_1[:,0], class_1[:,1],c='r',
-			    marker='+')
-	
-		c2 = plt.scatter(class_2[:,0], class_2[:,1],c='g',
-			    marker='o')
-	
-
-		c3 = plt.scatter(class_3[:,0], class_3[:,1],c='b',
-			    marker='*')
-		if centroids!=[]:
-			plt.scatter(centroids[:,0],centroids[:,1],c='k',marker='x')
-		plt.title('Iris dataset with 3 clusters and unknown outcomes : Iter '+str(iter))
-	
-	plt.pause(2)
 
 def euclidian(x1,x2):
 	return (np.sum((x1-x2)**2))
@@ -54,8 +16,10 @@ def getclass(datapoint, centroids):
 
 def compute_centroids(data, centroids):
 	classes = np.zeros(data.shape[0])
+	# assign each data point to closest centroid
 	for i in range(len(data)):
 		classes[i] = getclass(data[i], centroids)
+
 	new_centroids = np.ndarray(centroids.shape)
 
 	for i in range(len(centroids)):
@@ -64,11 +28,46 @@ def compute_centroids(data, centroids):
 
 	return new_centroids, classes
 
+def plotme(data, target, centroids=[], legends=False, iter=None):
+	plt.clf() # Draw new figure each time
+
+	# Ref: http://www.dummies.com/programming/big-data/data-science/how-to-visualize-the-clusters-in-a-k-means-unsupervised-learning-model/
+	class_1 = data[target==0]
+	class_2 = data[target==1]
+	class_3 = data[target==2]
+
+
+	
+	if legends:
+		c1 = plt.scatter(class_1[:,0], class_1[:,1],c='m',
+			    marker='+')
+		c2 = plt.scatter(class_2[:,0], class_2[:,1],c='m',
+			    marker='o')
+		c3 = plt.scatter(class_3[:,0], class_3[:,1],c='m',
+			    marker='*')
+		plt.legend([c1, c2, c3], ['Setosa', 'Versicolor',
+	    			'Virginica'])
+		plt.title('Iris dataset with 3 clusters and known outcomes')
+
+	else:
+		c1 = plt.scatter(class_1[:,0], class_1[:,1],c='r',
+			    marker='+')	
+		c2 = plt.scatter(class_2[:,0], class_2[:,1],c='g',
+			    marker='o')
+		c3 = plt.scatter(class_3[:,0], class_3[:,1],c='b',
+			    marker='*')
+
+		if centroids!=[]:
+			plt.scatter(centroids[:,0],centroids[:,1],c='k',marker='x')
+
+		plt.title('Iris dataset with 3 clusters and unknown outcomes : Iter '+str(iter))
+	
+	plt.pause(2)
 
 
 def kmeans(data, targets, k=2, iters=10,plotdata=None):
 
-
+	# chooses k random different points from the dataset as intitial centroid
 	centroids = random.sample(range(data.shape[0]),k)
 	print(centroids)
 	centroids =  np.array([data[i] for i in centroids])
@@ -86,11 +85,12 @@ if __name__ == '__main__':
 
 	iris = load_iris()
 
-	# Reduce iris data from 4 dimensionsto 2 for ease of plotting
+	# Reduce iris data from 4 dimensions to 2 for ease of plotting
 
 	pca = PCA(n_components=2).fit(iris.data)
 	pca_2d = pca.transform(iris.data)
-	plt.ion()
+
+	plt.ion() # Interactive on
 	
 	#You can change the first argument to iris.data and remove the centroid argument in every plotme call
 	kmeans(pca_2d, iris.target, k=len(np.unique(iris.target)), plotdata=pca_2d)
